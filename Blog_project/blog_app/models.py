160de743e1ff1e075c9d4848883e1c10a1a40a95
Preeti Kushwaha <preeti.kushwaha@vin.net>
2024-07-20 13:55:23 +0530

    create models and serilizers

--- Blog_project/blog_app/models.py
+++ Blog_project/blog_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User 
+from datetime import datetime
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=300)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    published_date = models.DateTimeField(datetime.now())
+    
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="Comments", on_delete = models.CASCADE)
+    author = models.CharField(max_length=100)
+    text = models.TextField(max_length=300)
+    created_date = models.DateField()
+    
+    def __str__(self):
+        return self.text
+    
+    
+    
+    
+    
